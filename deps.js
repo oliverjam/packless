bf75acb35927c5f80b738351de808f8d67557aeb
oliverjam <oliverjamesphillips@gmail.com>
2022-05-31 23:03:21 +0100

    Scaffold initial DB structure with tests

--- deps.js
+++ deps.js
@@ -1,3 +1,4 @@
 export { serve } from "https://deno.land/std@0.138.0/http/server.ts";
 export { extname, join } from "https://deno.land/std@0.138.0/path/mod.ts";
 export { render } from "./lib/jsx/render.js";
+export { DB } from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
