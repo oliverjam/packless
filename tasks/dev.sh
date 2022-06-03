c712a50b0690e6177356132e6305940e722f6c51
oliverjam <oliverjamesphillips@gmail.com>
2022-06-03 09:21:43 +0100

    FIx start scripts

--- tasks/dev.sh
+++ tasks/dev.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env sh
 
-EXPORT DB_PATH='db.sqlite'
+set -e
 
+DB_PATH='db.sqlite' \
 deno run \
   --allow-net=0.0.0.0:8000 \
   --allow-read=./public,db.sqlite,db.sqlite-journal,lib/database/schema.sql \
   --allow-write=db.sqlite \
   --allow-env=DB_PATH \
-  --watch
+  --watch \
   start.js
