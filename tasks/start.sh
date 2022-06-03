8d20dfc6948ce916c07c3b316e66ba4c80292554
oliverjam <oliverjamesphillips@gmail.com>
2022-06-03 09:47:12 +0100

    Need write access to SQLite journal

--- tasks/start.sh
+++ tasks/start.sh
@@ -6,6 +6,6 @@ DB_PATH='db.sqlite' \
 deno run \
   --allow-net=0.0.0.0:8000 \
   --allow-read=./public,db.sqlite,db.sqlite-journal,lib/database/schema.sql \
-  --allow-write=db.sqlite \
+  --allow-write=db.sqlite,db.sqlite-journal \
   --allow-env=DB_PATH \
   start.js
